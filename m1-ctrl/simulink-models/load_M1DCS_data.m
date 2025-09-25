9dc994aa980cf4babade6eb8880bc62b537d6258
Rodrigo Alvite Romano <rovite@gmail.com>
2025-09-25 13:58:41 -0300

    m1 control model update to match test cell design

--- m1-ctrl/simulink-models/load_M1DCS_data.m
+++ m1-ctrl/simulink-models/load_M1DCS_data.m
@@ -3,10 +3,13 @@
 %
 % Load variables to compile M1 controller
 % 
-% Apr, 2023: Simulink settings in an external file (Matlab 2022b)
-% Jan, 2023: Segment-wise implementation
-% Apr, 2022: Split decoupling matrices
+% Sep-2025: Outer force loop controller update (design employed in the test
+% cell experiment). Calibration matrices based on new structural model IOs.
+% Apr-2023: Simulink settings in an external file (Matlab 2022b)
+% Jan-2023: Segment-wise implementation
+% Apr-2022: Split decoupling matrices
 
+clearvars
 % Flag to compile M1 control model codes at the end of data loading process
 auto_compile = false;
 % Flag to save/update test data file
@@ -14,73 +17,175 @@ update_test_dt = false;%true; %
 % Flag to save/update controller data file
 update_calib_dt = false;%true; %
 
+%% Load M1 system data
 %%
-load('controls_5pt1g1K_z30_llTT_oad.mat','m1sys','fem');
+SAdataFolder = [];
+load(fullfile(SAdataFolder,'SupportActuatorArrayConfig.mat'),...
+    'OA_Kbal','OA_Kred','CS_Kbal','CS_Kred');
 
+% load('controls_5pt1g1K_z30_llTT_oad.mat','m1sys','fem');
 % m1sys variable content (seg = {1,...,7}):
 %
 % m1sys{seg}.HPstiff: Hardpoint stiffness
-% m1sys{seg}.LC2CG: HP: F&M convertion matrix
-% m1sys{seg}.ofl.SSdtC{ich}: M1 outer force loop controller
+% m1sys{seg}.M1RBM2HP: M1 RBM to HP length transformation <- redefined here
+% m1sys{seg}.LC2CG: HP: F&M convertion matrix <- M_HP recomputed here
+% m1sys{seg}.ofl.SSdtC{ich}: M1 outer force loop controller <- redefined here
 % m1sys{seg}.HPdtf: M1 HP length closed-loop dynamics
 % m1sys{seg}.Kbal: Load balancing
 % m1sys{seg}.Kred: Matrix to remove lateral force entries for Single SA
 % m1sys{seg}.m1BM2F: matrix to map BM into SA forces
 
 
+%% Load model static gain file
+%%
+
+% ===>>> User shall check file path and name
+if ismac(), mFolder = im.lfFolder;
+else, mFolder = '/home/rromano/mnt';
+end
+
+tel_model_label = "20250516_1420_zen_30_M1_202110_FSM_202305_Mount_202305_pier_202411_M1_actDamping";
+FileFolder = fullfile(mFolder, tel_model_label);
+
+if(~exist('inputTable','var') || 0)
+    try
+        staticSolFile = fullfile(FileFolder,"static_reduction_model.mat");
+        try
+            load(staticSolFile,...
+                'gainMatrixMountControlled','inputTable','outputTable');
+            gainMatrix = gainMatrixMountControlled;
+        catch
+            load(staticSolFile,'gainMatrix');
+        end
+    catch
+        load_static_model = 0;
+        warning('Unable to load static gain matrix\n')
+    end
+end
+
+
 %% Calibration-dependent matrices
 %%
 
-% LC to CG force transformation
-OA_LC2CG = zeros(6);
-for i_ = 1:6
-    OA_LC2CG = OA_LC2CG + (1/6) * m1sys{i_}.LC2CG;
+% HP-LC to CG force transformation
+M_HP_OA = zeros(6);
+M_HP_seg = cell(6,1);
+for i_ = 1:7    % Loop over all segments
+    % HP LC output indices
+    ind_HP_F = outputTable{'OSS_Hardpoint_force',"indices"}{1}((i_-1)*6+(1:6));
+    % M1 SA force input indices
+    ind_act_F = inputTable{sprintf('M1_actuators_segment_%d',i_),"indices"}{1};
+
+    % Static gain matrix from M1 SA force to HP LC measurement output
+    G2_bar = gainMatrix(ind_HP_F,ind_act_F);
+    
+    if(i_ < 7)
+        M_HP_seg{i_} = eye(6)/(G2_bar*OA_Kbal);
+        M_HP_OA = M_HP_OA + (1/6)*M_HP_seg{i_};
+    else, M_HP_CS = eye(6)/(G2_bar*CS_Kbal);
+    end
 end
-% Test if average LC2CG matrix is acceptable
-for i_ = 1:6
-    assert(norm(OA_LC2CG-m1sys{i_}.LC2CG)/norm(OA_LC2CG) < 0.001) ;
+
+% Test if average M_HP matrix is acceptable for OA
+for i_ = 1:6    % Loop over outer-axis segments
+    assert(norm(M_HP_OA-M_HP_seg{i_})/norm(M_HP_OA) < 0.001) ;
 end
-CS_LC2CG = m1sys{7}.LC2CG;
 
 
-% The script used to compute the controller parameters
-% (build_5ptYX_ctrl_interf.m) ensures that the HP stiffness saved as
-% m1sys{iS}.HPstiff is the same for any iS={1,...,7}.
-m1_HPk = m1sys{1}.HPstiff;
+% HP equivalent stiffness
+ind_cell = find(kron(ones(7,1),[ones(6,1);zeros(6,1)]));
+ind_face = find(kron(ones(7,1),[zeros(6,1);ones(6,1)]));
+
+inHP_F_ind = inputTable{'OSS_Harpoint_delta_F','indices'}{1};
+outHP_Da = outputTable{'OSS_Hardpoint_D',"indices"}{1}(ind_cell);   % face side
+outHP_Db = outputTable{'OSS_Hardpoint_D',"indices"}{1}(ind_face);   % cell side
+
+HPstiffvec = 1./diag(...
+        gainMatrix(outHP_Da,inHP_F_ind)-gainMatrix(outHP_Db,inHP_F_ind));
+fprintf('HP stiffness:%.3g +/-%.2g [N/um]\n',...
+    1e-6*mean(HPstiffvec),3e-6*std(HPstiffvec));
+m1_HPk = mean(HPstiffvec);
+
 
 % M1 RBM to HP length convertion
-S1_M1RBM2HP = m1sys{1}.M1RBM2HP;
-S2_M1RBM2HP = m1sys{2}.M1RBM2HP;
-S3_M1RBM2HP = m1sys{3}.M1RBM2HP;
-S4_M1RBM2HP = m1sys{4}.M1RBM2HP;
-S5_M1RBM2HP = m1sys{5}.M1RBM2HP;
-S6_M1RBM2HP = m1sys{6}.M1RBM2HP;
-S7_M1RBM2HP = m1sys{7}.M1RBM2HP;
+for i_ = 1:7    % Loop over all segments
+    seg_ind = (i_-1)*6+(1:6);
+    rbm_out_ind = outputTable{'OSS_M1_lcl',"indices"}{1}(seg_ind);
+    hplen_in_ind = inputTable{'OSS_Hardpoint_extension',"indices"}{1}(seg_ind);
+    eval(sprintf('S%d_M1RBM2HP = eye(6)/gainMatrix(rbm_out_ind,hplen_in_ind);',i_));
+end
 
+% Save calibration data matrices
 if update_calib_dt
 save('../calib_dt/m1_ctrl_dt.mat',...
-    'OA_LC2CG', 'CS_LC2CG', ...
+    'M_HP_OA', 'M_HP_CS', ...
     'm1_HPk',...
     'S1_M1RBM2HP', 'S2_M1RBM2HP', 'S3_M1RBM2HP', 'S4_M1RBM2HP',...
     'S5_M1RBM2HP', 'S6_M1RBM2HP', 'S7_M1RBM2HP');
 end
 
 
-%% Controller&Filter parameters
-% Support actuator dynamics (same for all segments)
-SA_dynNum = m1sys{1}.SAdyn.num{1};
-SA_dynDen = m1sys{1}.SAdyn.den{1};
+%% Controller & Actuator dynamics 
+%%
+% M1 control system sampled at 100Hz
+T_ofl = 0.01;   % [s]
 
-OA_ofl_SSdtC = m1sys{1}.ofl.SSdtC;
-CS_ofl_SSdtC = m1sys{7}.ofl.SSdtC;
+% Support actuator dynamics (same for all segments)
+H_p_cyl = 0.2/1 *tf([1 1*2*pi],[1 0.2*2*pi]) * tf(10*2*pi,[1 10*2*pi]);
+H_p_cyl_DT = c2d(H_p_cyl, T_ofl, 'foh');  
+SA_dynNum = H_p_cyl_DT.num{1};
+SA_dynDen = H_p_cyl_DT.den{1};
+
+% OFL controller: PI+Lead-lag (SPIE2024-)
+fPI = tf([0.1 5],[1 0]);
+flead = tf([1 0.1*2*pi],[1 0.3*2*pi]);
+flag = tf([1 15*2*pi],[1 5*2*pi]);
+fbH = balreal(ss(fPI * flead * flag));
+
+ofl_SSdtC = c2d(fbH, T_ofl, 'foh');
+OA_ofl_SSdtC = ofl_SSdtC;
+CS_ofl_SSdtC = ofl_SSdtC;
+
+% Hardpoint dynamics
+HDIm = 8.9e-4;      %[Kg m^2] Electric Motor Moment of Inertia
+Pv = tf(1,[HDIm 0]);    % Velocity inner loop TF
+HPKw2Lin = 1e-3/(161*(2*pi));   %[m/rads] Motor shaft to Linear speed
+Pp = tf(HPKw2Lin, [1 0]);   % Position plant (roller screw)
+
+% Controller gains to match test-cell data
+kpp_ = 3.1583e+07;
+kpi_ = 6.5797e+06;
+kvp_ = 0.0307562;
+kvi_ = 0.0153781;
+Cv_ = tf([kvp_, kvi_],[1 0]);
+Hv_ = minreal(Pv*Cv_/(1+Pv*Cv_));
+HPdyn = c2d(feedback(tf([kpp_, kpi_],[1 0])*Hv_*Pp,1), T_ofl,'foh');
+
+if(false)
+    G_CT = fbH;
+    hbode = bodeoptions;
+    hbode.FreqUnits = 'Hz';
+    hbode.XLabel.FontSize = 11;
+    hbode.YLabel.FontSize = 11;
+    hbode.Ylabel.String{1} = 'Mag';
+    figure(7000)
+    G_DT = c2d(G_CT, T_ofl, 'foh');
+    G_DT2 = c2d(G_CT, T_ofl, 'Tustin');
+    bode(G_CT, G_DT, G_DT2, hbode);
+    grid on;
+    xlim([0.05, 0.5/ofl.Ts])
+    set(gcf,'Position',[600   567   1.6*304*3/2   420*0.95]);
+    legend('CT','FOH','Tustin','Location','northwest'); legend box off;
+end
 
 
 %% Load simulink model
+%%
 ModelFName = 'M1DCS_2_rust';
 open(sprintf('%s.slx',ModelFName));
 
-build_subsys = 'M1_SA';
-% build_subsys = 'HP_dyn';
+% build_subsys = 'M1_SA';
+build_subsys = 'HP_dyn';
 
 hplc_label = sprintf('%s/M1_HP_loadcells', ModelFName);
 hp_RBMtoD_label = sprintf('%s/M1RBM_to_HP_relD', ModelFName);
@@ -109,28 +214,25 @@ cd(currentFolder);
 switch build_subsys
     case 'M1_SA'
         n_bm = 27; %size(m1sys{2}.m1BM2F,2);
-        % Solver sampling period
-        deltaT = m1sys{1}.ofl.Ts;
+        deltaT = OA_ofl_SSdtC.Ts;   % Solver sampling period
+
         set_param(hplc_label,'Commented','on');
         set_param(hp_RBMtoD_label,'Commented','on');
         set_param(hp_dyn_label,'Commented','on');
         set_param(hp_stiff_label,'Commented','on');
         set_param(m1SA_C_OA_label,'Commented','off');
         set_param(m1SA_C_CS_label,'Commented','off');
+        
         % Test data
-        for i1 = 1:numel(m1sys{1}.ofl.SSdtC)
-            OActrl(i1,i1) = m1sys{1}.ofl.SSdtC{i1}; %#ok<*SAGROW>
-            CSctrl(i1,i1) = m1sys{7}.ofl.SSdtC{i1};
-        end
-        OAsys = m1sys{1}.SAdyn*m1sys{1}.Kbal*OActrl;
-        CSsys = m1sys{7}.SAdyn*m1sys{7}.Kbal*CSctrl;
+        OAsys = H_p_cyl_DT * OA_Kbal * (eye(6)*OA_ofl_SSdtC);
+        CSsys = H_p_cyl_DT * CS_Kbal * (eye(6)*CS_ofl_SSdtC);
         [OAact_imp_y,OAact_imp_t] = impulse(OAsys);
         [CSact_imp_y,CSact_imp_t] = impulse(CSsys);
 
         if (update_test_dt && ~exist('m1_act_impulse_test','var'))
             save m1_act_impulse_test OAact_imp_t OAact_imp_y CSact_imp_t CSact_imp_y
         else
-            warning('Update test data feature is disabled!')
+            warning("Update test data feature is disabled!")
         end
 
         if auto_compile
@@ -144,8 +246,8 @@ switch build_subsys
         end
         
     case 'HP_dyn'
-        % Solver sampling period
-        deltaT = m1sys{1}.HPdtf.Ts;
+        deltaT = HPdyn.Ts; % Solver sampling period
+
         set_param(hplc_label,'Commented','off');
         set_param(hp_RBMtoD_label,'Commented','off');
         set_param(hp_dyn_label,'Commented','off');
@@ -154,7 +256,7 @@ switch build_subsys
         set_param(m1SA_C_CS_label,'Commented','on');
         
         % Test data
-        [hp_dyn_step_y,hp_dyn_step_t] = step(m1sys{1}.HPdtf);
+        [hp_dyn_step_y,hp_dyn_step_t] = step(HPdyn);
         if (update_test_dt && ~exist('hp_dyn_step_test','var'))
             save hp_dyn_step_test hp_dyn_step_t hp_dyn_step_y
         else
@@ -177,7 +279,7 @@ end
 %     % Dimension of the M1 bending modes command vector
 %     n_bm = 27; %#ok<UNRCH>
 %     
-%     load('controls_5pt3a_rr.mat','m1sys','fem');
+%     load('controls_5pt3a_rr.mat','m1sys');
 %     % m1sys{seg}.HPstiff: Hardpoint stiffness
 %     % m1sys{seg}.LC2CG: HP: F&M convertion matrix
 %     % m1sys{seg}.ofl.SSdtC{ich}: M1 outer force loop controller
